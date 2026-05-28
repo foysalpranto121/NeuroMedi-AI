f4357e25adb670132ab5c16464f2755fc6b1fea6
foysalpranto121 <foysalpranto2002@gmail.com>
2026-05-29 03:12:48 +0600

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Foysal Ahmed Pranto",
+    author_email="foysalpranto2002@gmail.com",
+    description="An AI-powered medical chatbot using LangChain and Pinecone",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.11",
+)
